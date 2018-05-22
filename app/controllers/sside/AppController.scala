89fefc7654f1851952abfb66575559f86efc0846
Alexander Strakh <strakh@ispras.ru>
2018-05-22 16:53:15 +0300

    Layout in progress

--- app/controllers/sside/AppController.scala
+++ app/controllers/sside/AppController.scala
@@ -1,4 +1,4 @@
-package controllers
+package controllers.sside
 
 import scala.concurrent.ExecutionContext
 
@@ -11,36 +11,32 @@ import play.api.mvc.AnyContent
 import play.api.mvc.ControllerComponents
 import play.api.mvc.Request
 import com.typesafe.config.Config
+import controllers.AppContext
+import controllers.Authorizable
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class AppController @Inject() (cc: ControllerComponents, dao: DAO, config: Config)(implicit ec: ExecutionContext)
-  extends AbstractController(cc)
-  with I18nSupport {
+  extends Authorizable(cc, dao, config) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+  import scala.concurrent.Future.{ successful => future }
 
-  def index = Action(Redirect(sside.routes.PostsController.posts(1)))
+  def index() = Action.async { implicit request =>
+    implicit val ac = new AppContext()
+    optionalAuthorized { accountOpt =>
+      future(Ok(views.html.app.index()))
+    }
+  }
+
+  //Action(Redirect(controllers.sside.routes.PostsController.posts(1)))
 
   //  def index() = Action { implicit request: Request[AnyContent] =>
   //    Ok(views.html.status())
   //  }
-
   def status = Action.async { implicit request =>
     implicit val ac = new AppContext()
     dao.getStats map { stats =>
       Ok(views.html.dev.status(stats))
     }
   }
-
 
 }
